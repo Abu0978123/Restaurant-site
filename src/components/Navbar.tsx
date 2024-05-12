5847dd4afdbd20b07aef2270fc320ebdda4754da
saudmercurydasha <saud@mercurydasha.com>
2024-05-12 09:06:48 +0500

    testing commits

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,6 +22,7 @@ const Navbar = () => {
             <li><button className="bg-color-secondary px-9 py-3 rounded-md capitalize font-bold hover:opacity-80 ease-in duration-200">free trial</button></li>
         </ul>
       </div>
+      {/* hamberger */}
     </nav>
   )
 }
